ff5e3f10f36bf7c7b69176713b3bd6e4b165ec84
vinceneto <vnetoxd@gmail.com>
2024-10-03 19:21:44 -0400

    adicionando classe matrix.py

--- uniform.py
+++ uniform.py
@@ -34,3 +34,5 @@ class Uniform(object):
             glUniform3f(self.variableRef, self.data[0], self.data[1], self.data[2])
         elif self.dataType == "vec4":
             glUniform4f(self.variableRef, self.data[0], self.data[1], self.data[2], self.data[3])
+        elif self.dataType == "mat4":
+            glUniformMatrix4fv(self.variableRef, 1, GL_TRUE, self.data)
